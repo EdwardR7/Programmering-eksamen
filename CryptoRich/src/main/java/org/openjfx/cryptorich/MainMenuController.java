ad5bba35b049a1ff422bf247cf003fc8dc3bf8eb
EdwardR7 <36159443+EdwardR7@users.noreply.github.com>
2022-01-24 13:53:02 +0100

    UserInterface, MenuBar etc., LineChart + label changeListener.. lists..

--- CryptoRich/src/main/java/org/openjfx/cryptorich/MainMenuController.java
+++ CryptoRich/src/main/java/org/openjfx/cryptorich/MainMenuController.java
@@ -6,14 +6,20 @@
 package org.openjfx.cryptorich;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 
 /**
  * FXML Controller class
  *
  * @author edwardrostomian
  */
-public class MainMenuController {
+public class MainMenuController implements Initializable  {
     
   @FXML
     private void switchToMinMainMenu() throws IOException {
@@ -24,11 +30,43 @@ public class MainMenuController {
         App.setRoot("KøbSælg");
     }
     
+   
+    @FXML
+    private ListView<String> AssetList;
+    
+    String[] Assets = {"Bitcoin","Ethereum","Cardano","Loopring","Solana", "Binance", "XRP","Dogecoin","Polkadot","SHIBA","Polygon","Polkadot","Cosmos","Chainlink","Algorand","Quant","MANA"};
+   
+    
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+    
+    AssetList.getItems().addAll(Assets);
+		
+		AssetList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+       
+                    @Override
+        public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+            
+                        try {
+                            throw new Exception("Not supported yet."); 
+                            
+                        } catch (Exception ex) {
+                            
+                            ex.printStackTrace();
+                        }
+        }
+
+
     
     //Simulation
+            
+                  
+                });
+}
     
-    
-    }
+}
+
+        
         
         
         
