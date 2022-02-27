5372d5ab649c354f82fb9f45c45461644aeee231
PeterDunlop <90600834+PeterDunlop@users.noreply.github.com>
2022-02-27 22:54:16 +0100

    Login System NO HASHING

--- CryptoRich/src/main/java/org/openjfx/cryptorich/DBMetoder.java
+++ CryptoRich/src/main/java/org/openjfx/cryptorich/DBMetoder.java
@@ -19,6 +19,8 @@ public class DBMetoder {
 
     private final String connectionString = "jdbc:sqlite:src/cryptoRichDB.db";
     public String UserExists;
+    public String verifyMessage;
+    public String LoginString;
 
     public void RegisterUser(User u, String userConfirm) throws SQLException, Exception {
         Connection conn = null;
@@ -28,6 +30,8 @@ public class DBMetoder {
         ResultSet rs = null;
         PreparedStatement ps = null;
 
+        //Uses imported driver manager to create a connection to database//
+        //The connection is put within a try catch, to ensure the program doesn't crash, if the connection fails, but instead a error message is returned//
         try {
             conn = DriverManager.getConnection(connectionString);
             System.out.println("Working db connection");
@@ -35,9 +39,12 @@ public class DBMetoder {
             System.out.println("DB Error: " + e.getMessage());
         }
 
+        //Since the id of a user cannot be 0, we don't check if the id is 0, we check that when upon a user is created, is the id of the user is not equal to -1 and if it is we create a user//
+        //In the registrecontroller we just set the users id to be -1 everytime, so that when it runs, it creates a new user everytime//
         if (u.getIdUser() != -1) {
 
         } else {
+            //Checks if string further down is already created, if it is, then don't do anything and return a error message, which can be inserted into a box in the program to show the user//
             if (UserExists == "Username already exists") {
                 UserExists = null;
             } else {
@@ -46,11 +53,13 @@ public class DBMetoder {
                     rs = ps.executeQuery();
 
                     if (rs.next()) {
+                        //Tests if the username exists and if it does, it sets the string to a message, which can be checked earlier//
                         UserExists = "Username already exists";
 
                     }
 
                 } catch (SQLException e) {
+                    //Returns this if the connection and resultset statements are broken in some kind of way/
                     System.out.println("Gone wrong");
 
                 }
@@ -58,7 +67,7 @@ public class DBMetoder {
                 try {
                     ps = conn.prepareStatement(sql2);
                     ps.executeUpdate();
-                   UserExists = "Succesful registration";
+                    UserExists = "Succesful registration";
 
                 } catch (SQLException e) {
                     System.out.println("DB Error: " + e.getMessage());
@@ -68,11 +77,66 @@ public class DBMetoder {
             }
         }
     }
-    
-    public boolean VerifyLogin(){
-    
-    return true;    
+
+    //Sets 2 strings for the username and passwords, then checks if these exist in the database//
+    //If the user or password does not exist, it returns false, and gives an error message//
+    //If the statement returns true, it sets a login string for a given user, which is meant to be hashed and changed and random//
+    //The login string is made random and hashed so nobody can figure out what user is joined at what time, not even the programmers, this string is specific and randome very time//
+    //Real system would require a checker if a login string is = another one, but since this is not an online project we only set 1 singular login string//
+    public boolean loginVerficiation(String username, String password) throws SQLException, Exception {
+        Connection conn = null;
+        Class.forName("org.sqlite.JDBC");
+        String sql = "SELECT * FROM User WHERE username = ? AND password = ?";
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+
+        //Connects to database//
+        try {
+            conn = DriverManager.getConnection(connectionString);
+            System.out.println("Working db connection");
+        } catch (SQLException e) {
+            System.out.println("DB Error: " + e.getMessage());
+        }
+
+        try {
+            //Allows us to insert a username from another class into our sql statements, which then can return a true or false value, based on whether our login statements match a user in the database//
+
+            //In this part of code, it checks if there is a username and password MATCHING, the reason it doesn't check if username is true or password is true is for safety//
+            //It is easier to check if a singular name is correct, than to check if both a username and password is matching, as in there are far more combinations and it is far harder to hack//
+            //Since the values will be hashed, it will ensure an even safer system//
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                verifyMessage = "Correct user and pass";
+                LoginString = "WorkingTest";
+                return true;
+            } else {
+
+                verifyMessage = "Username or password is incorrect";
+                LoginString = "Not working";
+                return false;
+            }
+        } catch (SQLException e) {
+            //Not written for user, for safety//
+
+            return false;
+        } finally {
+            conn.close();
+        }
     }
-    
+
 }
 
+// INVOLVER OM NAVNET EKSISTERER ALLEREDE OG TILFØJ FEJL BESKED//
+/*
+        try{
+            u.getIdUser() =
+             
+        }
+        
+                 
+        }
+ */
--- CryptoRich/src/main/java/org/openjfx/cryptorich/SecondaryController.java
+++ CryptoRich/src/main/java/org/openjfx/cryptorich/SecondaryController.java
@@ -2,8 +2,20 @@ package org.openjfx.cryptorich;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SecondaryController {
+        @FXML
+    private TextField txtLoginUsername, txtLoginPassword;
+
+    @FXML
+    private Label MessageBoxLogin;
+    
+    
+    private String EmptyFieldChecker;
+    
+    
     
       @FXML
     private void switchToRegistre() throws IOException {
@@ -15,4 +27,25 @@ public class SecondaryController {
         App.setRoot("MainMenu");
     }
     
+        @FXML
+    private void handleBtnLogin() throws IOException, Exception {
+        DBMetoder DB = new DBMetoder();
+        if(!txtLoginUsername.getText().isBlank() && !txtLoginPassword.getText().isBlank()){
+            if(DB.loginVerficiation(txtLoginUsername.getText(), txtLoginPassword.getText())){
+              MessageBoxLogin.setText(DB.verifyMessage);
+            }else{
+              MessageBoxLogin.setText(DB.verifyMessage);
+                
+            }
+            
+        }else if(txtLoginUsername.getText().isBlank()){
+            EmptyFieldChecker = "No username inserted";
+            MessageBoxLogin.setText(EmptyFieldChecker);
+        }else if(txtLoginPassword.getText().isBlank()){
+            EmptyFieldChecker = "No password inserted";
+            MessageBoxLogin.setText(EmptyFieldChecker);
+        }
+
+    }
 }
+
